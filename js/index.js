8cb2d5ade006bbfe5e67180176ccc91f49fe5ebc
suyarkhonuzb <suyarxonuzb007@gmail.com>
2023-10-03 20:12:31 +0500

    Add modal js

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const elModal = document.querySelector('.modal');
+const elModalClose = elModal.querySelector('.modal-close');
+
+
+elModalClose.addEventListener('click', function(){
+    elModal.classList.remove('modal-open')
+});
